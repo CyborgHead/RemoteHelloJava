42f629b5003c2daee4c9a8e47b8422f4908b5777
Amr Hisham <info@amrhisham.com>
2015-07-21 21:48:59 +0200

    Version 1.0.0 Revised

--- RemoteHelloJava/doc/db_helpers.sql
+++ RemoteHelloJava/doc/db_helpers.sql
@@ -8,3 +8,37 @@ INSERT INTO weatherstats (stat_date, wind_speed)
 VALUES ('2000-01-01', 15.0);
 
 DELETE FROM weatherstats WHERE id = 2;
+
+
+-----------------------------
+
+CREATE DATABASE codeclinic
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'English_United States.1252'
+       LC_CTYPE = 'English_United States.1252'
+       CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE codeclinic
+  IS 'Testing JDBC as well as solving the Code-Clinic Problems.';
+
+  
+CREATE TABLE weatherstats
+(
+  stat_date date NOT NULL, -- Record Date.
+  wind_speed real, -- Given Wind Speed.
+  id serial NOT NULL, -- A unique auto-incremental identifier for records.
+  CONSTRAINT weatherstats_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE weatherstats
+  OWNER TO postgres;
+COMMENT ON TABLE weatherstats
+  IS 'Stores weather stats imported from text files or via web-services.';
+COMMENT ON COLUMN weatherstats.stat_date IS 'Record Date.';
+COMMENT ON COLUMN weatherstats.wind_speed IS 'Given Wind Speed.';
+COMMENT ON COLUMN weatherstats.id IS 'A unique auto-incremental identifier for records.';
+
